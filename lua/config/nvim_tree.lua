46d5bfb20e202a5bfa15703a1c5dee6dceee76b0
lzy <1183720927@qq.com>
2021-09-14 14:50:45 +0800

    icon

--- lua/config/nvim_tree.lua
+++ lua/config/nvim_tree.lua
@@ -1,4 +1,34 @@
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
+vim.g.nvim_tree_icons = {
+  ['default']='',
+  ['symlink']= '',
+  ['git'] = {
+    ['unstaged']  = "✗",
+    ['staged']    = "✓",
+    ['unmerged']  = "",
+    ['renamed']   = "➜",
+    ['untracked'] = "★",
+    ['deleted']   = "",
+    ['ignored']   = "◌"
+    },
+  ['folder']= {
+    ['arrow_open']   = "",
+    ['arrow_closed'] = "",
+    ['default']      = "",
+    ['open']         = "",
+    ['empty']        = "",
+    ['empty_open']   = "",
+    ['symlink']      = "",
+    ['symlink_open'] = "",
+    },
+  ['lsp'] = {
+      ['hint']= "",
+      ['info']= "",
+      ['warning']= "",
+      ['error']= "",
+    }
+}
     -- default mappings
 vim.g.nvim_tree_bindings = {
   { key = {"<CR>", "o", "<2-LeftMouse>","l"}, cb = tree_cb("edit") },
